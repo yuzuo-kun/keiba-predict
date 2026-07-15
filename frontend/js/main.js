9c7add7afef89aeec12b2026a3384fdd0fa741f9
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-15 18:03:31 +0900

    main.js を更新

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,24 @@
-a
+const button = document.getElementById("predictBtn");
+
+button.addEventListener("click", async () => {
+
+    const url = document.getElementById("raceUrl").value;
+
+    const response = await fetch("https://あなたのRenderのURL.onrender.com/predict", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            url: url
+        })
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+
+    document.getElementById("result").textContent =
+        JSON.stringify(result, null, 2);
+
+});
