7e05669499ae978b2ae122d6389b1c230c2c926b
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-15 18:08:01 +0900

    predict.py を更新

--- backend/api/predict.py
+++ backend/api/predict.py
@@ -1,1 +1,11 @@
-a
+from fastapi import APIRouter
+from models.request import PredictRequest
+
+router = APIRouter()
+
+@router.post("/predict")
+def predict(request: PredictRequest):
+    return {
+        "status": "success",
+        "received_url": request.url
+    }
