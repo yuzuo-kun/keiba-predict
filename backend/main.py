568a03b72540849633e127782ecf031b3a6a6a04
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-15 18:15:19 +0900

    main.py を更新

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.predict import router as predict_router
 
 app = FastAPI(title="Keiba Predict API")
+
+origins = [
+    "https://keiba-predict-frontend.onrender.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(predict_router)
 
