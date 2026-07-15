85b4c7eda2ec2b2738f204a0aa552797233d3913
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-15 18:37:24 +0900

    predict.py を更新

--- backend/api/predict.py
+++ backend/api/predict.py
@@ -1,11 +1,23 @@
 from fastapi import APIRouter
+from fastapi.responses import Response
+
 from models.request import PredictRequest
+from scraper.fetcher import fetch_html
 
 router = APIRouter()
 
+
 @router.post("/predict")
 def predict(request: PredictRequest):
-    return {
-        "status": "success",
-        "received_url": request.url
+
+    html = fetch_html(request.url)
+
+    headers = {
+        "Content-Disposition": 'attachment; filename="race.html"'
     }
+
+    return Response(
+        content=html,
+        media_type="text/html; charset=utf-8",
+        headers=headers
+    )}
