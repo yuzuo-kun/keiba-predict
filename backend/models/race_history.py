a495d70856cbf3d817783df83c5880b411373f29
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-16 07:43:57 +0900

    race_history.py を更新

--- backend/models/race_history.py
+++ backend/models/race_history.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 
 
 class RaceHistory(BaseModel):
-    race_place str | None
-    direction str | None
-    distance: int              # 距離(m)
-    time: str                  # タイム
-    last3f: float | None       # 上がり
-    first_corner: int | None   # 初回コーナー位置
-    final_corner: int | None   # 最終コーナー位置
+    race_place str | None     # 場所
+    direction str | None      # 方向
+    distance: int | None      # 距離(m)
+    race_horce_no | None      # 馬番
+    time: str | None          # タイム
+    last3f: float | None      # 上がり
+    first_corner: int | None # 初回コーナー位置
+    final_corner: int | None  # 最終コーナー位置
