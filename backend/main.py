f45342d9fb7f2abd7e15a7e4bd3959f129c9f12b
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-15 18:23:58 +0900

    main.py を更新

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.predict import router as predict_router
 
 app = FastAPI(title="Keiba Predict API")
+
+# CSS・JavaScript配信用
+app.mount("/static", StaticFiles(directory="../frontend"), name="static")
 
 origins = [
     "https://keiba-predict-frontend.onrender.com"
