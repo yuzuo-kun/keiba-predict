68813195a3e5147c6a70d543c698c8d163c0f9b3
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-15 18:36:40 +0900

    fetcher.py を更新

--- backend/scraper/fetcher.py
+++ backend/scraper/fetcher.py
@@ -1,1 +1,11 @@
-a
+import requests
+
+
+def fetch_html(url: str) -> str:
+    response = requests.get(url)
+
+    response.raise_for_status()
+
+    response.encoding = response.apparent_encoding
+
+    return response.text
