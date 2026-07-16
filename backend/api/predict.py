57f80a47933f0a39a447c0c34602e6a27e8cad46
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-16 18:24:05 +0900

    predict.py を更新

--- backend/api/predict.py
+++ backend/api/predict.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter
 
+from analyzer.calculator import calculate
 from models.request import PredictRequest
 from models.response import PredictResponse
+from scraper.extractor import extract_info
 from scraper.fetcher import fetch_html
 from scraper.parser import parse_html
-from scraper.extractor import extract_info
 
 router = APIRouter()
 
 
 @router.post("/predict")
-def predict(request: PredictRequest) -> PredictResponse:
+def predict(request: PredictRequest):
+
     html = fetch_html(request.url)
     soup = parse_html(html)
     info = extract_info(soup)
-    
-    return PredictResponse(info=info)
+
+    distance = calculate(info)
+
+    return PredictResponse(
+        info=info,
+        distance=distance
+    )
