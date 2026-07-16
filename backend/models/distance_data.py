4e2c58f764b128458385f33380a24f5b03ff2749
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-16 18:03:14 +0900

    distance_data.py を更新

--- backend/models/distance_data.py
+++ backend/models/distance_data.py
@@ -1,1 +1,18 @@
-あ
+from typing import Optional
+
+from pydantic import BaseModel
+
+from models.history import history
+
+
+class distance_data(BaseModel):
+    race_place: str
+    distance: int
+
+    histories: list[history]
+
+    avg_time: Optional[str] = None
+    avg_last3f: Optional[float] = None
+    best_last3f: Optional[float] = None
+    avg_first_corner_diff: Optional[float] = None
+    avg_final_corner_diff: Optional[float] = None
