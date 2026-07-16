f8d19b141bcc35129dd6baefd487b14c0f6283c5
yuzuki.H <130666721+yuzuo-kun@users.noreply.github.com>
2026-07-16 18:30:43 +0900

    distance_data.py を更新

--- backend/models/distance_data.py
+++ backend/models/distance_data.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel
 
-from models.race_history import race_history
+from models.race_history import RaceHistory
 
 
 class distance_data(BaseModel):
